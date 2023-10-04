6c4c9b529bdb42e4a168a2a0ae6e4895e0092e71
mikedecr <mgdecrescenzo@gmail.com>
2023-10-03 22:50:37 -0500

    exports

--- R/currr.R
+++ R/currr.R
@@ -1,4 +1,5 @@
 
+#' @export
 currify_verb <- function(verb) {
     function(...) {
         intention <- function(.data) {
@@ -8,15 +9,24 @@ currify_verb <- function(verb) {
     }
 }
 
+#' @export
 `%.%` <- function(g, f) function(...) g(f(...))
+#' @export
 `%|%` <- function(f, g) g %.% f
 
 selecting   <- currify_verb(dplyr::select)
+#' @export
 filtering   <- currify_verb(dplyr::filter)
+#' @export
 grouping    <- currify_verb(dplyr::group_by)
+#' @export
 ungrouping  <- currify_verb(dplyr::ungroup)
+#' @export
 mutating    <- currify_verb(dplyr::mutate)
+#' @export
 summarizing <- currify_verb(dplyr::summarize)
+#' @export
 arranging   <- currify_verb(dplyr::arrange)
+#' @export
 renaming    <- currify_verb(dplyr::rename)
 
